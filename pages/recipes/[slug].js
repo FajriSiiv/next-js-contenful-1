fb36192f229851b2c18dc8027adda8b3a5349b55
Muhammad Fajri <fajrijijin@gmail.com>
2021-08-17 21:40:15 +0700

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
@@ -19,7 +20,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 export async function getStaticProps({ params }) {
@@ -33,6 +34,7 @@ export async function getStaticProps({ params }) {
   };
 }
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
   console.log(recipe.fields);
   return (
